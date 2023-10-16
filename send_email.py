800357d49ff0a83d1daead2b7d6879d37c850183
mithul <mithul.ashokkumar@gmail.com>
2023-10-16 09:54:23 -0600

    Securing our password

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
@@ -7,7 +8,7 @@ def send_email(message):
     port = 465
 
     username = "mithul.ashokkumar@gmail.com"
-    password = "xbkkdsmxetwktyrj"
+    password = os.getenv("PASSWORD")
 
     receiver = "mithul.ashokkumar@gmail.com"
     context = ssl.create_default_context()
